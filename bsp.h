d91b9b59d69f63a0fdffeac9607f5608de904743
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-07-10 22:26:23 +0200

    Removes constant from bsp

--- bsp.h
+++ bsp.h
@@ -1,7 +1,7 @@
 #ifndef BSP_H
 #define BSP_H
 
-static const int BUZZER_PIN = 28;
+#define BUZZER_PIN 28
 
 void __attribute__((noreturn)) __assert_func(const char *file, int line, const char *func, const char *failedexpr);
 void play_tone(float frequency);
