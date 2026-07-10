a58b8e799cf2dfac635fc4fd1e6c5307726f60c5
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-07-10 13:04:48 +0200

    Adds proper parsing functions to peek and advance the current position

--- sound.c
+++ sound.c
@@ -183,6 +183,26 @@ void play_tone(uint pin, float frequency) {
     pwm_set_enabled(slice_num, true);
 }
 
+char peek(const char *str, int *pos) {
+    while (str[*pos] == ' ') {
+        (*pos)++;
+    }
+
+    return str[(*pos)];
+}
+
+char advance(const char *str, int *pos) {
+    if (peek(str, pos) != '\0') (*pos)++;
+}
+
+void advance_after(const char *str, int *pos, const char to_skip) {
+    for (char c = peek(str, pos); c != '\0' && c != to_skip; c = peek(str, pos)) {
+        advance(str, pos);
+    }
+
+    advance(str, pos);
+}
+
 int main()
 {
     stdio_init_all();
@@ -193,38 +213,35 @@ int main()
     int pos = 0;
 
     // Skip the name
-    while(song[pos] != ':') pos++;
-    pos++;
+    advance_after(song, &pos, ':');
 
-    pos += 2;
+    // TODO handle end of string error
+
+    advance_after(song, &pos, '=');
     int default_duration = atoi(&song[pos]);
-    while(song[pos] != ',') pos++;
-    pos++;
+    advance_after(song, &pos, ',');
 
-    pos += 2;
+    advance_after(song, &pos, '=');
     int default_octave = atoi(&song[pos]);
-    while(song[pos] != ',') pos++;
-    pos++;
+    advance_after(song, &pos, ',');
 
-    pos += 2;
+    advance_after(song, &pos, '=');
     int bpm = atoi(&song[pos]);
     float ms_per_note = (60.0f * 1000.0f * 4.0f) / bpm;
-    while(song[pos] != ':') pos++;
-    pos++;
+    advance_after(song, &pos, ':');
 
     while (pos < strlen(song)) {
         int duration = atoi(&song[pos]);
+        duration = duration ? duration : default_duration;
 
-        if (duration == 0) {
-            duration = default_duration;
-        }
-
-        while('0' <= song[pos] && song[pos] <= '9') pos++;
+        for (char c = peek(song, &pos); c != '\0' && '0' <= c && c <= '9'; c = peek(song, &pos)) {
+            advance(song, &pos);
+        };
 
         int  note           = P;
         bool can_have_sharp = false;
 
-        switch (song[pos]) {
+        switch (peek(song, &pos)) {
             case 'a':
                 note = A;
                 can_have_sharp = true;
@@ -262,32 +279,35 @@ int main()
                 note = P;
 
                 break;
+            default:
+                // TODO handle error
         }
 
-        pos++;
+        advance(song, &pos);
 
-        if (can_have_sharp && song[pos] == '#') {
+        if (can_have_sharp && peek(song, &pos) == '#') {
+            advance(song, &pos);
+
             note++;
-            pos++;
         }
 
         float duration_ms = ms_per_note / duration;
 
-        if (song[pos] == '.') {
+        if (peek(song, &pos) == '.') {
+            advance(song, &pos);
+
             duration_ms *= 1.5f;
-            pos++;
         }
 
         int octave = atoi(&song[pos]);
+        octave = octave ? octave : default_octave;
 
-        if (octave == 0) {
-            octave = default_octave;
+        for (char c = peek(song, &pos); c != '\0' && '0' <= c && c <= '9'; c = peek(song, &pos)) {
+            advance(song, &pos);
         }
 
-        while('0' <= song[pos] && song[pos] <= '9') pos++;
-
-        if (song[pos] == ',') {
-            pos++;
+        if (peek(song, &pos) == ',') {
+            advance(song, &pos);
         }
 
         float frequency = FREQUENCY_LUT[octave * 13 + note];
