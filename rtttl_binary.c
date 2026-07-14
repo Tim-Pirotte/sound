bcd427ce8725a4a4cfe12190e43b5b5d0f7ddae5
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-07-14 18:56:20 +0200

    Adds a script to calculate occurence probabilities of title characters and tone commands

--- rtttl_binary.c
+++ rtttl_binary.c
@@ -11,10 +11,20 @@
 // Title
 // 126 ASCII values + 1 terminator (':' cannot be used in the title in the text format)
 // = 127 values
+//
+// Encoding:
+//   Terminator: 0 = 0
+//   ASCII: 1..126 = 1 + ASCII value
 
 // Tone commands
-// 880 (20..900) bpm values + (6 duration values * 4 octave values * 13 notes * (1 has dot + 1 no dot)) + 1 terminator
-// 1505
+// 880 (20..900) bpm values + (6 duration values * 4 octave values * 12 notes * (1 has dot + 1 no dot)) + 6 duration values for pause + 1 terminator
+// = 1463 values
+//
+// Encoding:
+//   Terminator: 0 = 0
+//   BPM: 1..880 = 1 + BPM - 20
+//   Pauses: 881..886 = 881 + duration index
+//   Notes: 887..1462 = 887 + duration index * 96 + note index * 8 + octave index * 2 + has dot
 
 // The dataset used for calculating the probabilities is the 10 000 Mixed Tunes 3 pack on:
 // https://picaxe.com/rtttl-ringtones-for-tune-command/
